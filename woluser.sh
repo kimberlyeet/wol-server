c7291a6ca49f65eff82d782dab5d1b28687261b7
kmz27 <44536631+kmz27@users.noreply.github.com>
2022-12-15 09:39:23 +0100

    Update woluser.sh

--- woluser.sh
+++ woluser.sh
@@ -1,5 +1,5 @@
 # by https://github.com/kmz27
 #!/bin/bash
 useradd -s /wol.sh -m $1 # Add new user with "wol.sh" as the default shell
-echo "$1:wakeywakey" | chpasswd # Set a default password
+# echo "$1:wakeywakey" | chpasswd # Set a default password (optional)
 touch /home/$1/.hushlogin # Create ".hushlogin" file to disable the login banner
