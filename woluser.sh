90f66db327aed753dd72256267e88b6a9462c6ad
kimberlyeet <44536631+kimberlyeet@users.noreply.github.com>
2024-04-26 13:35:03 +0200

    remove comments

--- woluser.sh
+++ woluser.sh
@@ -1,4 +1,3 @@
-# by https://github.com/kmz27
 #!/bin/bash
 useradd -s /wol.sh -m $1 # Add new user with "wol.sh" as the default shell
 echo "$1:wakeywakey" | chpasswd # Set a default password (optional)
