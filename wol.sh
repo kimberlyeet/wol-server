fd7158eb5f2533f5d1fb53860ba506b8009592ff
kimberlyeet <44536631+kimberlyeet@users.noreply.github.com>
2024-04-26 13:34:38 +0200

    remove comments

--- wol.sh
+++ wol.sh
@@ -1,3 +1,2 @@
-# by https://github.com/kmz27
 #!/bin/bash
 wakeonlan $(echo $USER |  sed -e 's/\([0-9A-Fa-f]\{2\}\)/\1:/g' -e 's/\(.*\):$/\1/') # Insert a colon every two characters to the username and send a wol packet to the mac.
